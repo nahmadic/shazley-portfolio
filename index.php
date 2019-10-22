9b1e11cc69143a3553794b7e08c6520e5746f33d
Ryan Browne <brownetown7@gmail.com>
2019-10-22 21:47:27 +0100

    Adds some initial structure

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+  <link rel="stylesheet" href="./styles/main.css">
+</head>
+<body>
+  <div class="page-wrapper">
+    <header class="header">
+      <div class="header__logo">
+        <h1><a href="/">Stuart Hazley</a></h1>
+      </div>
+      <div class="header__nav">
+        <nav class="navigation">
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/contact.php">Contact</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  </div>
+</body>
+</html>
