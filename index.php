dc4068ddc1a8ff6710af19a327df7531bd688777
Ryan Browne <brownetown7@gmail.com>
2019-10-27 17:12:46 +0000

    Adds new structure to homepage

--- index.php
+++ index.php
@@ -20,6 +20,72 @@
         </ul>
       </nav>
     </header>
+    <section class="content">
+      <div class="content__inner">
+        <div class="font">
+          <div class="font-number">
+            <p>01.</p>
+          </div>
+          <div class="font-name">
+            <div class="font-name__title">
+              <p>OSTENT FAMILY</p>
+            </div>
+            <div class="font-name__description">
+              <p>A Din inspired font family. <br />Produced 2019</p>
+            </div>
+            <div class="font-name__links">
+              <a href="https://www.myfonts.com/fonts/stuart-hazley/ostent/">- Purchase from MyFonts</a>
+              <a href="https://www.youworkforthem.com/font/T11199/ostent">- Purchase from YWFT</a>
+            </div>
+          </div>
+          <div class="font-details">
+            <div class="font-details__demo">
+              <h5>Ostent Heavy</h5>
+              <p class="font--ostent-heavy">ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopstuvwxyz 0123456789</p>
+            </div>
+            <div class="font-details__demo">
+              <h5>Ostent Regular</h5>
+              <p class="font--ostent-regular">ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopstuvwxyz 0123456789</p>
+            </div>
+            <div class="font-details__demo">
+              <h5>Ostent Light</h5>
+              <p class="font--ostent-light">ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopstuvwxyz 0123456789</p>
+            </div>
+          </div>
+        </div>
+        <div class="font">
+          <div class="font-number">
+            <p>02.</p>
+          </div>
+          <div class="font-name">
+            <div class="font-name__title">
+              <p>RB INSIGNIA</p>
+            </div>
+            <div class="font-name__description">
+              <p>A custom logo font. <br />Produced 2018</p>
+            </div>
+          </div>
+          <div class="font-details">
+            <div class="font-details__demo">
+              <h5>RB Insignia Regular</h5>
+              <p class="font--rb-logo">RB</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="social">
+      <div class="social__icons">
+        <a href="#" class="social__icon social__icon--twitter"></a>
+        <a href="#" class="social__icon social__icon--instagram"></a>
+        <a href="#" class="social__icon social__icon--linkedin"></a>
+      </div>
+    </section>
+    <footer class="footer">
+      <div class="footer__inner">
+        <p>© STUART HAZLEY 2019</p>
+      </div>
+    </footer>
   </div>
 </body>
 </html>
